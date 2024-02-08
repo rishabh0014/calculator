a6134dc97220c5d6baeb09b5643b14221ba8d2d6
rishabh0014 <rishabhteli14@gmail.com>
2024-02-08 17:46:15 +0530

    Done

--- src/main/java/org/sp/miniProject/Calculator.java
+++ src/main/java/org/sp/miniProject/Calculator.java
@@ -14,12 +14,12 @@ public class Calculator {
 
         Calculator calculator = new Calculator();
         Scanner scanner = new Scanner(System.in);
-        double number1, number2,result;
-
+        double number1, number2;
+//         System.out.println("Scientific Calculator using DevOps.");
         do {
-            System.out.println("Basic Calculator using DevOps.");
+            System.out.println("Scientific Calculator using DevOps.");
             System.out.println("Choose operation : ");
-            System.out.print("1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n" +
+            System.out.print("1. Factorial\n2. Square root\n3. Power\n4. Natural Logarithm\n" +
                     "5. Exit\nEnter your choice: ");
             int choice;
             try {
@@ -69,6 +69,7 @@ public class Calculator {
             }
         } while (true);
     }
+
 
     public double factorial(double number1) {
         logger.info("[FACTORIAL] - " + number1);
